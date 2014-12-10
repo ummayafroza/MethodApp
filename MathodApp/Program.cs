859074f33bf2b882edfa76ae91ad18a1f74a53dc
ummayafroza <ummayafroza31@gmail.com>
2014-12-10 15:28:27 +0600

    Method added

---
+++ MathodApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double i = 10;
+            int a = 100;
+            long b = a;
+
+            long c = 30;
+
+            int d = (int)c;
+
+
+            double result = GetSquare(i);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static double GetSquare(double aValue)
+        {
+            double squareValue = aValue * aValue;
+            return squareValue;
+
+        }
+    }
+}
